a14694f451dbd70e46523fe6f55757fb15b09769
Dima Barsukov <oldreddemon@gmail.com>
2019-01-20 18:57:06 +0300

    [ADD] Node Server

---
+++ server.js
@@ -0,0 +1,29 @@
+// TODO: Import HTML maket
+
+//====IMPORTS====
+// var info = require('./js/getInfo');
+// var ejs = require('ejs');
+var express = require('express');
+var app = express();
+
+
+//====VARS====
+var ip = 'localhost';
+var port = 4958;
+
+
+//====CODE====
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+  res.send("It is main");
+});
+
+app.get("/:car", function (req, res) {
+  res.render('item', {
+    model: req.params.car
+  });
+});
+
+app.listen(port);
+console.log(ip + ":" + port);
